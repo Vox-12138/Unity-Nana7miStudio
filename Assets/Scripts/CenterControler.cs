f472d07715d550614fc11869fe09f7126a93a390
Vox <840505226@qq.com>
2022-02-22 13:35:01 +0800

    最终更新

---
+++ Assets/Scripts/CenterControler.cs
@@ -0,0 +1,202 @@
+﻿using Live2D.Cubism.Framework;
+using Live2D.Cubism.Framework.LookAt;
+using Live2D.Cubism.Framework.MouthMovement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CenterControler : MonoBehaviour
+{
+    private Animator animator;
+    private CubismEyeBlinkController eye_blink_controler;
+    private CubismLookController look_controler;
+    private CubismMouthController mouth_controller;
+    private AudioSource audio_source;
+
+    private int motion_num;
+    private int un_motion_num;
+    private int gn_motion_num;
+
+    private bool special_mode;
+
+    // 容纳动画音源
+    public AudioClip[] audio_clips;
+    public AudioClip[] un_audio_clips;
+    public AudioClip[] gn_audio_clips;
+
+    // 列表切换按钮
+    public GameObject ToggleSpecialModeOn;
+    private Toggle toggle_special_mode_on;
+
+    /// <summary>
+    /// 以下是函数
+    /// </summary>
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 30帧足矣
+        Application.targetFrameRate = 30;
+
+        // 动画机
+        animator = 
+            this.GetComponent<Animator>();
+
+        // 自动眨眼
+        eye_blink_controler = 
+            this.GetComponent<CubismEyeBlinkController>();
+
+        // 目光跟随
+        look_controler = 
+            this.GetComponent<CubismLookController>();
+
+        // 口型适配
+        mouth_controller =
+            this.GetComponent<CubismMouthController>();
+        mouth_controller.enabled = false;
+
+        // 播放列表切换键的按钮组件
+        toggle_special_mode_on =
+            ToggleSpecialModeOn.GetComponent<Toggle>();
+
+        // 音源
+        audio_source =
+            this.GetComponent<AudioSource>();
+
+        // 动画播放序号
+        motion_num = Random.Range(1, audio_clips.Length + 1);
+        un_motion_num = Random.Range(1, un_audio_clips.Length + 1);
+        gn_motion_num = 1;
+
+        // 切换播放列表
+        special_mode = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // 播放动画
+    public void Motion_play()
+    {
+        // 序号循环
+        if (special_mode)
+        {
+            un_motion_num += 1;
+            if (un_motion_num > un_audio_clips.Length)
+            {
+                un_motion_num = 1;
+            }
+
+            // 动画播放
+            animator.SetInteger("un_motion_num", un_motion_num);
+        }
+        else
+        {
+            motion_num += 1;
+            if (motion_num > audio_clips.Length)
+            {
+                motion_num = 1;
+            }
+
+            // 动画播放
+            animator.SetInteger("motion_num", motion_num);
+        }
+    }
+
+    // 动画开始播放时调用
+    public void Motion_start()
+    {
+        // 关闭自动眨眼和目光跟随，打开口型适配
+        eye_blink_controler.enabled = false;
+        look_controler.enabled = false;
+        mouth_controller.enabled = true;
+
+        // 播放音源
+        if (special_mode)
+        {
+            audio_source.clip = un_audio_clips[un_motion_num - 1];
+            audio_source.Play();
+        }
+        else
+        {
+            audio_source.clip = audio_clips[motion_num - 1];
+            audio_source.Play();
+        }
+
+        // 回到idle动画
+        animator.SetInteger("motion_num", 0);
+        animator.SetInteger("un_motion_num", 0);
+        animator.SetInteger("gn_motion_num", 0);
+    }
+
+    // 动画结束播放时调用
+    public void Motion_end()
+    {
+        eye_blink_controler.enabled = true;
+        look_controler.enabled = true;
+        mouth_controller.enabled = false;
+
+        animator.SetInteger("motion_num", 0);
+        animator.SetInteger("un_motion_num", 0);
+        animator.SetInteger("gn_motion_num", 0);
+    }
+
+    // 切换播放列表
+    public void Switch_Special_Mode()
+    {
+        special_mode = toggle_special_mode_on.isOn;
+
+        animator.SetBool("special_mode", special_mode);
+
+    }
+
+    /// <summary>
+    /// 以下是GN部分
+    /// </summary>
+
+    // 播放GN动画
+    public void GN_Motion_play()
+    {
+        // 序号循环
+        if (special_mode)
+        {
+            gn_motion_num += 1;
+            if (gn_motion_num > gn_audio_clips.Length)
+            {
+                gn_motion_num = 1;
+            }
+
+            // 动画播放
+            animator.SetInteger("gn_motion_num", gn_motion_num);
+        }
+    }
+
+    // 动画开始播放时调用
+    public void GN_Motion_start()
+    {
+        // 打开口型适配
+        mouth_controller.enabled = true;
+
+        // 播放音源
+        audio_source.clip = gn_audio_clips[gn_motion_num - 1];
+        audio_source.Play();
+
+        // 回到idle动画
+        animator.SetInteger("motion_num", 0);
+        animator.SetInteger("un_motion_num", 0);
+        animator.SetInteger("gn_motion_num", 0);
+    }
+
+    // 动画结束播放时调用
+    public void GN_Motion_end()
+    {
+        mouth_controller.enabled = false;
+
+        animator.SetInteger("motion_num", 0);
+        animator.SetInteger("un_motion_num", 0);
+        animator.SetInteger("gn_motion_num", 0);
+    }
+
+}
---
+++ Assets/Scripts/VideoControler.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoControler : MonoBehaviour
+{
+    // 播放器
+    private VideoPlayer video_player;
+
+    // 播放视频的序号
+    private int for_num;
+    private int unfor_num;
+
+    // 标志播放的曲目的模式
+    private bool special_mode = false;
+    private bool now_special_mode = false;
+
+    // 视频
+    public VideoClip[] forwardable;
+    public VideoClip[] unforwardable;
+
+    // BV号
+    public string[] forwardable_bv_num;
+    public string[] unforwardable_bv_num;
+
+    // 播放按钮
+    public GameObject TogglePlayVideo;
+    private Toggle toggle_play_video;
+
+    // 列表切换按钮
+    public GameObject ToggleSpecialModeOn;
+    private Toggle toggle_special_mode_on;
+
+    /// <summary>
+    /// 以下是函数
+    /// </summary>
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // VideoPlayer组件
+        video_player =
+            this.GetComponent<VideoPlayer>();
+
+        // 设置初始音量
+        video_player.SetDirectAudioVolume(trackIndex: 0, 0);
+
+        // 视频播放序号
+        for_num = Random.Range(0, forwardable.GetLength(0));
+        unfor_num = Random.Range(0, unforwardable.GetLength(0));
+        video_player.clip = forwardable[for_num];
+
+        // 播放&暂停键的按钮组件
+        toggle_play_video =
+            TogglePlayVideo.GetComponent<Toggle>();
+
+        // 播放列表切换键的按钮组件
+        toggle_special_mode_on =
+            ToggleSpecialModeOn.GetComponent<Toggle>();
+
+        // 每个视频结束时自动播放下一曲
+        video_player.loopPointReached += Auto_next;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 调节音量
+    public void Set_volume(float volume)
+    {
+        video_player.SetDirectAudioVolume(trackIndex : 0, volume);
+    }
+
+    // 播放&暂停
+    public void Video_play_pause()
+    {
+        if (video_player.isPlaying)
+        {
+            video_player.Pause();
+        }
+        else
+        {
+            video_player.Play();
+        }
+    }
+
+    // 切换播放列表
+    public void Switch_Special_Mode()
+    {
+        special_mode = toggle_special_mode_on.isOn;
+    }
+
+    // 下一曲
+    public void Video_next()
+    {
+        if (special_mode)
+        {
+            unfor_num += 1;
+            if (unfor_num > unforwardable.GetLength(0) - 1)
+            {
+                unfor_num = 0;
+            }
+
+            video_player.clip = unforwardable[unfor_num];
+
+            now_special_mode = true;
+        }
+        else
+        {
+            for_num += 1;
+            if (for_num >  forwardable.GetLength(0) - 1)
+            {
+                for_num = 0;
+            }
+
+            video_player.clip = forwardable[for_num];
+
+            now_special_mode = false;
+        }
+
+        toggle_play_video.isOn = true;
+    }
+
+    //上一曲
+    public void Video_last()
+    {
+        if (special_mode)
+        {
+            unfor_num -= 1;
+            if (unfor_num < 0)
+            {
+                unfor_num = unforwardable.GetLength(0) - 1;
+            }
+
+            video_player.clip = unforwardable[unfor_num];
+
+            now_special_mode = true;
+        }
+        else
+        {
+            for_num -= 1;
+            if (for_num < 0)
+            {
+                for_num = forwardable.GetLength(0) - 1;
+            }
+
+            video_player.clip = forwardable[for_num];
+
+            now_special_mode = false;
+        }
+
+        toggle_play_video.isOn = true;
+    }
+
+    // 打开网页
+    public void Open_web()
+    {
+        if (now_special_mode)
+        {
+            Application.OpenURL
+                (
+                "https://www.bilibili.com/video/" + unforwardable_bv_num[unfor_num]
+                );
+        }
+        else
+        {
+            Application.OpenURL
+               (
+               "https://www.bilibili.com/video/" + forwardable_bv_num[for_num]
+               );
+        }
+    }
+
+    // 自动下一曲
+    void Auto_next(VideoPlayer vp)
+    {
+        if (special_mode)
+        {
+            unfor_num += 1;
+            if (unfor_num > unforwardable.GetLength(0) - 1)
+            {
+                unfor_num = 0;
+            }
+
+            vp.clip = unforwardable[unfor_num];
+
+            now_special_mode = true;
+        }
+        else
+        {
+            for_num += 1;
+            if (for_num > forwardable.GetLength(0) - 1)
+            {
+                for_num = 0;
+            }
+
+            vp.clip = forwardable[for_num];
+
+            now_special_mode = false;
+        }
+
+        toggle_play_video.isOn = true;
+    }
+
+    // 打开B站自动暂停
+    public void Auto_pause()
+    {
+        toggle_play_video.isOn = false;
+
+        video_player.Pause();
+    }
+}
